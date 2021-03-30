f88c49c83cf3e721cab0eec11439e734429684e0
Ernest Gaisie <ernestyawgaisie@gmail.com>
2021-03-30 15:24:45 +0000

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAlcIfreMJgu559haGfwnQHPQNIPi7APGg",
+  authDomain: "gaisie-facebook-clone.firebaseapp.com",
+  databaseURL: "https://gaisie-facebook-clone.firebaseio.com",
+  projectId: "gaisie-facebook-clone",
+  storageBucket: "gaisie-facebook-clone.appspot.com",
+  messagingSenderId: "88520313838",
+  appId: "1:88520313838:web:c8c0cdbd6b9e2dee311cff",
+  measurementId: "G-L2V1K5QQ17",
+};
