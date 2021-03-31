c5f3cb465a40dbf96f128c3d25c6140a7d1b1c46
Ernest Gaisie <ernestyawgaisie@gmail.com>
2021-03-31 13:12:44 +0000

    Header component done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Header from "./components/header/Header";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+
+      <div className="app__body">
+        <Sidebar />
+      </div>
+    </div>
+  );
 }
 
 export default App;
